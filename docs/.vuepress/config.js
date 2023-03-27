40815e06693045f3edd9b4249a7a2ba05d94999c
liuzhiqiang100 <liuzhiqiang100@crdigital.com.cn>
2023-03-27 23:01:10 +0800

    第一次提交

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,14 +4,30 @@ const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
   theme: 'vdoing', // 使用npm包主题
-  title: "vdoing-template",
+  title: "clarkliu1314.github.com",
   description: 'vdoing博客主题模板',
-  base: '/vdoing-template/', // 格式：'/<仓库名>/'， 默认'/'
+  base: '/clarkliu1314.github.com/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
   },
 
   head,
-  plugins,
+// vssue 评论插件
+  plugins: [
+    [
+      "vuepress-plugin-vssue-global",
+      {
+        platform: "github",
+        title: "[Comment]<%- frontmatter.title %>",
+        needComments: true,
+        // 其他的 Vssue 配置
+        autoCreateIssue: true,
+        clientId: "cbef7b888f407be1dad7",
+        clientSecret: "7702eb21d19ebb94173ad8b3a7ae657aea184999",
+        owner: "clarkliu1314",
+        repo: "clarkliu1314.github.com",
+      },
+    ],
+  ],
   themeConfig,
 }
